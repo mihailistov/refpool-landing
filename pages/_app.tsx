5163cac4fc884ea160602e217f06d3a7a0e6adee
Mihai Listov <mihailistov@gmail.com>
2025-07-31 01:27:01 -0700

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <title>refpool — Scale Your Marketing, Reward Top Creators</title>
         <meta
           name="description"
